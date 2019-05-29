ea9f9a1fdaa778716367c4ed04d7d7224ca9b6d4
FengYen-Chang <a60207john@gmail.com>
2019-05-29 12:48:20 +0800

    v0.1 main

--- C++/main.cpp
+++ C++/main.cpp
@@ -77,19 +77,6 @@ int main(int argc, char *argv[])
     plugin_d.AddExtension(std::make_shared<Extensions::Cpu::CpuExtensions>());
 
     CNNNetReader network_reader_d, network_reader_e;
-    
-    // ------------------------------- DECODER
-
-    network_reader_d.ReadNetwork(FLAGS_m_d);
-    std::string binFileName_d = fileNameNoExt(FLAGS_m_d) + ".bin";
-    network_reader_d.ReadWeights(binFileName_d);
-
-    network_reader_d.getNetwork().setBatchSize(1);
-    CNNNetwork network_d = network_reader_d.getNetwork();
-
-    auto input_info_d = network_d.getInputsInfo().begin()->second;
-    auto input_name_d = network_d.getInputsInfo().begin()->first;
-    input_info_d->setInputPrecision(Precision::FP32);
 
     // ------------------------------- ENCODER
 
@@ -100,12 +87,159 @@ int main(int argc, char *argv[])
     network_reader_e.getNetwork().setBatchSize(1);
     CNNNetwork network_e = network_reader_e.getNetwork();
 
+    std::vector<std::string> inputsName_e;
+    InputsDataMap inputsInfo_e(network_e.getInputsInfo());
+    size_t input_w, input_h;
+    for (auto & item : inputsInfo_e)
+    {
+        item.second->setPrecision(Precision::U8);
+        inputsName_e.push_back(item.first);
+        input_w = item.second->getDims()[0];
+        input_h = item.second->getDims()[1];
+    }
 
+    std::vector<std::string> outputsName_e;
+    OutputsDataMap outputsInfo_e(network_e.getOutputsInfo());
+    for (auto & item : outputsInfo_e)
+    {
+        item.second->setPrecision(Precision::FP32);
+        outputsName_e.push_back(item.first);
+    }
+    
+    // ------------------------------- DECODER
 
+    network_reader_d.ReadNetwork(FLAGS_m_d);
+    std::string binFileName_d = fileNameNoExt(FLAGS_m_d) + ".bin";
+    network_reader_d.ReadWeights(binFileName_d);
 
+    network_reader_d.getNetwork().setBatchSize(1);
+    network_reader_d.getNetwork().addOutput("60")
+    CNNNetwork network_d = network_reader_d.getNetwork();
+
+    std::size_t hidden_state_size;
+    
+    std::vector<std::string> inputsName_d;
+    std::vector<InputInfo::Ptr> inputsData_d;
+    // std::vector<DataPtr> inputs;
+    InputsDataMap inputsInfo_d(network_d.getInputsInfo());
+    // for (auto & item : inputsInfo_d)
+    for (std::size_t i = 0; i < inputsInfo_d.size(); i++)
+    {   
+        inputsInfo_d[i].second->setPrecision(Precision::FP32);
+        inputsName_d.push_back(inputsInfo_d[i].first);
+        
+        if (i == 1)
+            hidden_state_size = inputsInfo_d[i].second->getDims()[0] * 
+                                inputsInfo_d[i].second->getDims()[1] * 
+                                inputsInfo_d[i].second->getDims()[2];
+    }
+
+    std::vector<std::string> outputsName_d;
+    OutputsDataMap outputsInfo_d(network_d.getOutputsInfo());
+    for (auto & item : outputsInfo_d)
+    {
+        item.second->setPrecision(Precision::FP32); 
+        outputsName_d.push_back(item.first);
+    }
+
+    // --------------------------------- EXECUTION ENCODER
+
+    auto executable_network_e = plugin.LoadNetwork(network_e, {});
+    auto infer_request_e = executable_network_e.CreateInferRequest();
+    
+    auto input_e = infer_request.GetBlob(inputsName_e[0]);
+    auto input_data_e = input_e->buffer().as<PrecisionTrait<Precision::U8>::value_type*>();
+
+    cv::Mat image = cv::imread(FLAGS_i);
+    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+    cv::resize(image, image, cv::Size(input_h, input_w), INTER_LANCZOS4);
+
+    size_t image_size = input_h * input_w;
+    
+    for(size_t pid = 0; pid < image_size; pid++)
+    {
+        for (size_t ch = 0; ch < 3; ch++)
+        {
+            input_data[ch * image_size + pid] = image.at<cv::Vec3b>(pid)[ch];
+        }        
+    }
+    
+    infer_request_e.Infer();
+
+    auto output_e = infer_request_e.GetBlob(outputsName_e[0]);
+    auto output_data_point_e = output_e->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
+
+    // --------------------------------- EXECUTION DECODER
+
+    auto executable_network_d = plugin.LoadNetwork(network_d, {});
+    auto infer_request_d = executable_network_d.CreateInferRequest();
+    
+    std::vector<int> sentence_ids;
+    
+    // cv::Mat init_state_h = cv::zeros(hidden_state_size[0], hidden_state_size[1], CV_32FC1);
+    // cv::Mat init_state_c = cv::zeros(hidden_state_size[0], hidden_state_size[1], CV_32FC1);
+    // (hidden_state_size[0], hidden_state_size[1], CV_32FC1, cv::S);
+
+    for (int text = 0; text < FLAGS_tl; text++)
+    {
+        if (text == 0)
+        {
+            infer_request_d.GetBlob(inputsName_d[0])
+                ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>() = 
+                infer_request_e.GetBlob(outputsName_e[0])
+                    ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
+            
+            for (size_t pid = 0; pid < hidden_state_size; pid++)
+            {
+                infer_request_d.GetBlob(inputsName_d[1])
+                    ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>()[pid] = 0.f;
+                infer_request_d.GetBlob(inputsName_d[2])
+                    ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>()[pid] = 0.f;
+            }
+
+        }
+        else
+        {
+            infer_request_d.GetBlob(inputsName_d[0])
+                ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>() =
+                infer_request_d.GetBlob(outputsName_d[2])
+                    ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
+
+            infer_request_d.GetBlob(inputsName_d[1])
+                ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>() =
+                infer_request_d.GetBlob(outputsName_d[0])
+                    ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
+        
+            infer_request_d.GetBlob(inputsName_d[2])
+                ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>() =
+                infer_request_d.GetBlob(outputsName_d[1])
+                    ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>();
+        }
+
+        infer_request_d.Infer();
+        sentence_ids.push_back(
+            infer_request_d.GetBlob(outputsName_d[3])
+                ->buffer().as<PrecisionTrait<Precision::FP32>::value_type*>()[0];
+        );
+
+        if (sentence_ids.back() == 2)
+            break;
+    }
+
+    for (int i = 0; i < sentence_ids.size(); i++)
+        cout << sentence_ids[i] << "\t";
+    cout << "\n";
+
+    std::vector<std::string> sampled_caption;
+    for (int i = 0; i < sentence_ids.size(); i++)
+        sampled_caption.push_back(vocab[sentence_ids[i]]);
+
+    for (int i = 0; i < sampled_caption.size(); i++)
+        cout << sampled_caption[i] << " ";
+    cout << "\n";
 
     
-
+    return 0;
 }//MAIN
 
 
